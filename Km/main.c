d36f902d24b79efb6b8e6e781c68f1d8ef691d41
chaimae mita <chaimae.mita@gmail.com>
2021-11-11 09:24:53 +0100

    Km

---
+++ Km/main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    float Km,Mile;
+
+    printf("donner le Mile: \n");
+    scanf("%f", &Mile);
+
+     Km = Mile/1.609;
+
+     printf("Le Km est: %f\n",Km);
+
+}

32d0a64a8d9cde7f5c022f923371955f7eeb00c1
chaimae mita <chaimae.mita@gmail.com>
2021-11-11 09:52:08 +0100

    LA TEMP

---
+++ LA TEMP/main.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    float F,C;
+
+    printf("La température en F: \n");
+    scanf("%f", &C);
+
+    C=(F-32)/1.8;
+
+    printf("La Temperature en C est: %f\n",C);
+    if (C<0)
+        printf("La Temperature est: Tres Froid \n");
+    else if (C<30)
+        printf("La Temperature est:  Froid \n");
+    else if(C=30)
+        printf("La Temperature est: chaud \n");
+    else if(C>30)
+        printf("La Temperature est: Tres chaud \n");
+
+    return 0;
+}

4973e38864398ce683645fd1e5342c6a2fb864c5
chaimae mita <chaimae.mita@gmail.com>
2021-11-12 15:30:37 +0100

    CHA 13

---
+++ CHA 13.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<math.h>
+#include<math.h>
+int main ()
+{
+	int nbr;
+	
+	printf("donner un nombre: ");
+	scanf("%d", &nbr);
+	printf("le nombre en hexadecimale: %x \n", nbr);
+	printf("le nombre en octale: %o ", nbr);
+	
+	return 0;
+}

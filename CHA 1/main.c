ae54fbb1bed1f4358fc09736e73e1aea4977e6e7
chaimae mita <chaimae.mita@gmail.com>
2021-11-12 11:45:07 +0100

    modification

---
+++ CHA 1/main.c
@@ -0,0 +1,29 @@
+
+#include <unistd.h>
+#include <stdio.h>
+
+int main(int argc, char **argv){
+
+    char nom[20], prenom[20],sexe[20];
+    int age[20],tele[20];
+
+    printf("Votre Nom\n");
+    scanf("%s", nom);
+
+    printf("Votre Prenom\n");
+    scanf("%s", prenom);
+
+    printf("Votre Age\n");
+    scanf("%s", age);
+
+    printf("Votre Sexe\n");
+    scanf("%s", sexe);
+
+    printf("Votre Tele\n");
+    scanf("%s", tele);
+
+
+    printf("Nom Complet : %s %s, Age : %s, Sexe : %s, Tele : %s\n", nom, prenom, age, sexe, tele);
+
+    return 0;
+}
